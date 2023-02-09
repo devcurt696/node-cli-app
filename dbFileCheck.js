528e3b2c85897e40411506ca67cafc95d81827b4
Curtis Crouse <72637696+curtcurt69@users.noreply.github.com>
2023-02-08 23:01:15 -0500

    node crud cli app

---
+++ dbFileCheck.js
@@ -0,0 +1,11 @@
+import fs from "fs";
+import { exit } from "process";
+
+export default function dbFileCheck() {
+    if (!fs.existsSync("db.json")) {
+        console.log("File does not exist!");
+        exit(1);
+    }
+}
+
+dbFileCheck();
